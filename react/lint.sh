2c295bed46904e632405c552d706834cd4d2ac0d
Petr Rajtslegr <petr.rajtslegr@gmail.com>
2020-11-16 13:34:08 +0100

    first commit

---
+++ react/lint.sh
@@ -0,0 +1,5 @@
+(
+npm install eslint-plugin-prettier eslint-config-prettier --save-dev;
+export PKG=eslint-config-airbnb;
+npm info "$PKG@latest" peerDependencies --json | command sed 's/[\{\},]//g ; s/: /@/g' | xargs npm install --save-dev "$PKG@latest"
+)
